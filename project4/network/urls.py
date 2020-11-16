0c7f8e4046ea1a4747e2e7c72f1330d122eca3ba
Hanxu Liang <sam.liang.wk@gmail.com>
2020-11-15 23:33:31 -0500

    Third file path adjustment

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -1,5 +1,5 @@
-
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
@@ -7,5 +7,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("profile/<str:username>", views.profile, name="profile"),
+    path("profile/<str:username>/newpost", views.newpost, name="newpost"),
+    path("<str:post_id>/delete", views.delete, name="delete"),
+    path("following/<str:username>", views.following, name="following"),
+    path("posts/<int:post_id>/edit", views.edit, name="edit"),
+    path("profile/settings/<str:username>", views.settings, name="settings"),
+    url(r'^likepost/$', views.likepost, name='like-post')
 ]
--- project4/network/views.py
+++ project4/network/views.py
@@ -1,26 +1,28 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-
-from .models import User
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import User, Post, Profile, Like
 
 
 def index(request):
-    return render(request, "network/index.html")
+    # putting all posts by users and in reverse so the latests posts come first
+    posts = Post.objects.all().order_by('id').reverse()
+    paginator = Paginator(posts, 10)  # making 10 posts at a page
+    page_number = request.GET.get('page')  # page per view
+    page_obj = paginator.get_page(page_number)
+    return render(request, "network/index.html", {'page_obj': page_obj})
 
 
 def login_view(request):
-    if request.method == "POST":
-
-        # Attempt to sign user in
+    if request.method == "POST":  # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-
-        # Check if authentication successful
-        if user is not None:
+        if user is not None:  # Check if authentication successful
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
@@ -28,7 +30,10 @@ def login_view(request):
                 "message": "Invalid username and/or password."
             })
     else:
-        return render(request, "network/login.html")
+        if request.user.is_anonymous:
+            return render(request, "network/login.html")
+        else:
+            return redirect('index')
 
 
 def logout_view(request):
@@ -40,17 +45,13 @@ def register(request):
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
-
-        # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
-        if password != confirmation:
+        if password != confirmation:  # Ensure password matches confirmation
             return render(request, "network/register.html", {
                 "message": "Passwords must match."
             })
-
-        # Attempt to create new user
-        try:
+        try:  # Attempt to create new user
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
@@ -58,6 +59,184 @@ def register(request):
                 "message": "Username already taken."
             })
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return redirect('settings', username)
     else:
-        return render(request, "network/register.html")
+        if request.user.is_authenticated:
+            return redirect(request, 'index')
+        else:
+            return render(request, "network/register.html")
+
+
+def profile(request, username):
+    if request.method == 'GET':  # clicking a user profile
+        currentuser = request.user
+        profileuser = get_object_or_404(User, username=username)
+        posts = Post.objects.filter(user=profileuser).order_by('id').reverse()
+        follower = Profile.objects.filter(target=profileuser)
+        following = Profile.objects.filter(follower=profileuser)
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            following_each_other = Profile.objects.filter(
+                follower=currentuser, target=profileuser)
+            totalfollower = len(follower)
+            totalfollowing = len(following)
+            paginator = Paginator(posts, 10)
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+            context = {
+                'posts': posts.count(),
+                'profileuser': profileuser,
+                'page_obj': page_obj,
+                'follower': follower,
+                'totalfollower': totalfollower,
+                'following': following,
+                'totalfollowing': totalfollowing,
+                'followingEachOther': following_each_other
+            }
+            return render(request, "network/profile.html", context)
+    else:  # clicking follow/unfollow button
+        currentuser = request.user
+        profileuser = get_object_or_404(User, username=username)
+        posts = Post.objects.filter(user=profileuser).order_by('id').reverse()
+        following_each_other = Profile.objects.filter(
+            follower=request.user, target=profileuser)
+        paginator = Paginator(posts, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        if not following_each_other:
+            follow = Profile.objects.create(
+                target=profileuser, follower=currentuser)
+            follow.save()
+            follower = Profile.objects.filter(target=profileuser)
+            following = Profile.objects.filter(follower=profileuser)
+            following_each_other = Profile.objects.filter(
+                follower=request.user, target=profileuser)
+            totalfollower = len(follower)
+            totalfollowing = len(following)
+
+            context = {
+                'posts': posts.count(),
+                'profileuser': profileuser,
+                'page_obj': page_obj,
+                'follower': follower,
+                'following': following,
+                'totalfollowing': totalfollowing,
+                'totalfollower': totalfollower,
+                'followingEachOther': following_each_other
+            }
+
+            return render(request, "network/profile.html", context)
+
+        else:
+            following_each_other.delete()
+            follower = Profile.objects.filter(target=profileuser)
+            following = Profile.objects.filter(follower=profileuser)
+            totalfollower = len(follower)
+            totalfollowing = len(following)
+
+            context = {
+                'posts': posts.count(),
+                'profileuser': profileuser,
+                'page_obj': page_obj,
+                'follower': follower,
+                'following': following,
+                'totalfollowing': totalfollowing,
+                'totalfollower': totalfollower,
+                'followingEachOther': following_each_other
+            }
+
+            return render(request, "network/profile.html", context)
+
+
+def newpost(request, username):
+    if request.method == 'POST':
+        user = get_object_or_404(User, username=username)
+        textarea = request.POST["textarea"]
+        if not textarea:  # if empty return to index
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        post = Post.objects.create(content=textarea, user=user)
+        post.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
+def delete(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
+def following(request, username):
+    if request.method == 'GET':
+        currentuser = get_object_or_404(User, username=username)
+        follows = Profile.objects.filter(follower=currentuser)
+        posts = Post.objects.all().order_by('id').reverse()
+        postlist = []
+        for p in posts:
+            for follower in follows:
+                if follower.target == p.user:
+                    postlist.append(p)
+        if not follows:
+            return render(request, 'network/following.html', {'message': "You haven't followed anyone!"})
+
+        paginator = Paginator(postlist, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'network/following.html', {'page_obj': page_obj})
+
+
+def edit(request, post_id):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=post_id)
+        textarea = request.POST["textarea"]
+        post.content = textarea
+        post.save()
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
+def settings(request, username):
+    user = request.user
+    if request.method == 'GET':
+        profile = User.objects.get(username=username)
+        if request.user.is_anonymous:
+            return redirect('login')
+        if profile.username == user.username:
+            return render(request, 'network/settings.html', {'profile': profile})
+        else:
+            return redirect("index")
+    else:
+        profile = User.objects.get(username=username)
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        email_exists = User.objects.filter(email=email)
+        profile.first_name = first_name
+        profile.last_name = last_name
+        if not email_exists or profile.email == email:
+            profile.email = email
+        else:
+            return render(request, "network/settings.html", {'profile': profile, 'message': '*Email already taked.'})
+
+        profile.save()
+        return redirect('profile', profile.username)
+
+
+def likepost(request):
+    user = request.user
+    if request.method == 'GET':
+        post_id = request.GET['post_id'] # get post id and check if post is in liked model and delete or add if needed
+        liked_post = Post.objects.get(pk=post_id)
+
+        if user in liked_post.liked.all():
+            liked_post.liked.remove(user)
+            like = Like.objects.get(post=liked_post, user=user)
+            like.delete()
+        else:
+            liked_post.liked.add(user)
+            liked_post.save()
+            like = Like.objects.get_or_create(post=liked_post, user=user)
+
+        return HttpResponse('success')
